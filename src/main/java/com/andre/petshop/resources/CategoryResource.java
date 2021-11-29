a14ff73555a4482b2f456ff50be51cbafdd01db0
andrelrpacheco <andrelrpacheco@gmail.com>
2021-11-28 21:04:50 -0300

    CRUD de categoria mais manipulação do exception

--- src/main/java/com/andre/petshop/resources/CategoryResource.java
+++ src/main/java/com/andre/petshop/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.andre.petshop.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.petshop.domain.Category;
 import com.andre.petshop.service.CategoryService;
@@ -22,5 +26,28 @@ public class CategoryResource {
 		Category categoryData = service.findCategoryId(id);
 		
 		return ResponseEntity.ok().body(categoryData);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> createCategory(@RequestBody Category data) {
+		
+		data = service.createCategory(data);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(data.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> updateCategory(@RequestBody Category data, @PathVariable Integer id) {
+		
+		data.setId(id);
+		data = service.updateCategory(data);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> removeCategory(@PathVariable Integer id) {
+		service.removeCategory(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/andre/petshop/service/CategoryService.java
+++ src/main/java/com/andre/petshop/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.andre.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.andre.petshop.domain.Category;
 import com.andre.petshop.repository.CategoryRepository;
+import com.andre.petshop.service.exceptions.DataIntegrityException;
 import com.andre.petshop.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,5 +20,24 @@ public class CategoryService {
 	public Category findCategoryId(Integer id) {
 		Optional<Category> categoryId = accessRepository.findById(id);
 		return categoryId.orElseThrow(() -> new ObjectNotFoundException("Categoria não encontrada!"));
+	}
+	
+	public Category createCategory(Category data) {
+		data.setId(null);
+		return accessRepository.save(data);
+	}
+	
+	public Category updateCategory(Category data) {
+		findCategoryId(data.getId());
+		return accessRepository.save(data);
+	}
+	
+	public void removeCategory(Integer id) {
+		findCategoryId(id);
+		try {
+			accessRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Categoria possui produto, não é possível deletar!");
+		}
 	}
 }
